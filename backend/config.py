52708197fc35ecb266bd7a7608455619ed7fe9bc
Ik Teng <154477174+ikteng@users.noreply.github.com>
2026-01-01 14:18:08 +0800

    Update

--- backend/config.py
+++ backend/config.py
@@ -5,6 +5,7 @@ import os
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_PUBLISHABLE_KEY")
+# SUPABASE_KEY = os.getenv("SUPABASE_PUBLISHABLE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_SECRET_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
